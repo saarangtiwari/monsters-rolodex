fca347a0837638a4a0c519c7f5ca315e1001dd06
Saarang Tiwari <saarang.tiwari@hotmail.com>
2019-08-05 21:24:57 +0530

    Updates

---
+++ precache-manifest.113259aa96f9e1de9c544b49be6092b6.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1093c1f8d99b78bed26ba209e0171afa",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "9a8b69aa4c9ac28a5a35",
+    "url": "/monsters-rolodex/static/css/main.e9ec0667.chunk.css"
+  },
+  {
+    "revision": "681b51057d74cfa8975d",
+    "url": "/monsters-rolodex/static/js/2.65aa1cca.chunk.js"
+  },
+  {
+    "revision": "9a8b69aa4c9ac28a5a35",
+    "url": "/monsters-rolodex/static/js/main.a4ca03d0.chunk.js"
+  },
+  {
+    "revision": "c3f3f5c37da57c0082d3",
+    "url": "/monsters-rolodex/static/js/runtime~main.c2844b1e.js"
+  }
+]);
